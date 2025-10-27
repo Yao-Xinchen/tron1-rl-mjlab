d0e2deb435010e3da862f30a3aa03a17f6161931
Yao-Xinchen <yao29@illinois.edu>
2025-10-27 21:48:15 +0800

    add wf_tron.py

---
+++ src/assets/wf_tron/wf_tron1a.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import mujoco
+
+from mjlab.entity import Entity, EntityCfg, EntityArticulationInfoCfg
+from mjlab.utils.spec_config import ActuatorCfg
+
+current_dir: Path = Path(__file__).parent.resolve()
+
+WF_TRON_XML: Path = current_dir / "xml" / "robot.xml"
+assert WF_TRON_XML.exists(), f"XML file not found: {WF_TRON_XML}"
+
+
+def get_spec() -> mujoco.MjSpec:
+    return mujoco.MjSpec.from_file(str(WF_TRON_XML))
+
+
+WF_TRON_LEG_ACTUATORS = ActuatorCfg(
+    joint_names_expr=["abad_[RL]_Joint", "hip_[RL]_Joint", "knee_[RL]_Joint"],
+    effort_limit=80.0,
+    stiffness=40.0,
+    damping=1.8,
+)
+
+WF_TRON_WHEEL_ACTUATORS = ActuatorCfg(
+    joint_names_expr=["wheel_[RL]_Joint"],
+    effort_limit=40.0,
+    stiffness=0.0,
+    damping=0.5,
+)
+
+WF_TRON_ARTICULATION = EntityArticulationInfoCfg(
+    actuators=(
+        WF_TRON_LEG_ACTUATORS,
+        WF_TRON_WHEEL_ACTUATORS,
+    ),
+)
+
+WF_TRON_ROBOT_CFG = EntityCfg(
+    spec_fn=get_spec,
+    articulation=WF_TRON_ARTICULATION,
+)
+
+if __name__ == "__main__":
+    import mujoco.viewer as viewer
+
+    robot = Entity(WF_TRON_ROBOT_CFG)
+    viewer.launch(robot.spec.compile())
