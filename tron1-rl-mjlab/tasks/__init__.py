20f7642b6e1d26e7324fd1aa3a5b91fab4f22918
Yao-Xinchen <yao29@illinois.edu>
2025-10-28 23:04:03 +0800

    rename src directory

--- tron1-rl-mjlab/tasks/__init__.py
+++ tron1-rl-mjlab/tasks/__init__.py
@@ -5,7 +5,7 @@ gym.register(
     entry_point="mjlab.envs:ManagerBasedRlEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.cfg.wf_tron_env_cfg:CartPoleEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.cfg.wf_tron_env_cfg:WfTronEnvCfg",
         "rl_cfg_entry_point": f"{__name__}.cfg.wf_tron_env_cfg:RslRlOnPolicyRunnerCfg",
     },
 )
